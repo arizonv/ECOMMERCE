3cf9bf050006406428f2efe02001c801dec1d1d4
arizonv <a.vasgarridoe@gmail.com>
2023-04-28 17:32:52 -0400

    BASE

---
+++ tienda/forms.py
@@ -0,0 +1,39 @@
+from tkinter import Widget
+from django import forms
+from .models import contacto,Producto
+
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.models import User
+
+
+class ContactoForm (forms.ModelForm):
+    class Meta:
+        model = contacto
+        widgets = {
+            'descripcion': forms.Textarea(attrs={'cols': 37, 'rows': 5 }),
+        }
+        fields = ["nombre" ,"apellido","email" ,"numero","descripcion",'region', 'comuna']
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+
+        model = Producto
+        widgets = {
+            'descripcion': forms.Textarea(attrs={'cols': 37, 'rows': 5 }),
+        }
+        fields = '__all__'
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ["username" ,"first_name","last_name","email","password1","password2"]
+        #fields = '__all__'
+
+
+
+
+
---
+++ tienda/views.py
@@ -0,0 +1,150 @@
+from email import message
+from pyexpat.errors import messages
+from django.forms import PasswordInput
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+import random
+from tienda.models import Categorias, Marca, Producto
+from .forms import ContactoForm,CustomUserCreationForm,ProductoForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.http import Http404
+import json
+
+from .serializers import productoSerializer
+from rest_framework import viewsets
+
+
+from django.db.models import Q
+
+
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset =Producto.objects.all()
+    serializer_class = productoSerializer
+
+
+
+
+def home(request):
+    return render(request, 'home.html', {'name': 'gvrrido'})
+
+
+
+
+def store(request):
+    busqueda = request.POST.get("buscador")
+    productos = Producto.objects.order_by('nombre')
+    if busqueda:
+        productos = Producto.objects.filter(
+            Q(nombre__icontains = busqueda) |
+            Q(descripcion__icontains = busqueda)
+        ).distinct()
+    try:
+        productos = productos
+    except:
+        raise Http404
+
+    data = {'entity': productos,
+            'title': 'LISTADO DE PRODUCTOS',
+            }
+    return render(request,'store.html', data)
+    
+def contacto(request):
+
+    with open('apiResources/regiones-comunas.json', encoding='utf-8') as f:
+        try:
+            rc = json.load(f)
+            print(rc)
+        except ValueError:
+            print("El archivo JSON está mal formateado")
+
+    regiones = [(r['name'], r['number']) for r in rc['regions']]
+    comunas = {r['number']: r['communes'] for r in rc['regions']}
+    form = ContactoForm()
+
+    if request.method == 'POST':
+        formulario = ContactoForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "contacto guardado"
+            form = ContactoForm()  # Crear una nueva instancia del formulario vacío
+        else:
+            form = formulario  # Asignar el formulario no válido a la variable 'form'
+            
+    return render(request, 'contacto.html', {'form': form, 'regiones': regiones, 'comunas': comunas})
+
+
+
+
+
+
+    
+def detalleProducto(request,id):
+    producto = get_object_or_404(Producto,id=id)
+    context = {
+        'producto': producto
+    }
+    return render(request, 'producto/detalle.html', context )
+
+
+
+
+def registro(request):  
+    data = {
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'post':
+        formulario = CustomUserCreationForm(data=request.POST)
+
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"],password=formulario.cleaned_data["password1"])
+            login (request,user)
+            messages.success(request, "Usurio Registrado!")
+            return redirect(to='home')
+        data ["form"] = formulario
+    return render(request, 'registration/registro.html', data )
+
+
+
+##################################################################################################################################################### VIEW PRODUCTO    #############################################################################
+
+
+
+def addProducto(request):
+    data = {
+        'form' : ProductoForm()
+    }
+
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, files=request.FILES)
+
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Registro agregado correctamente")
+            return redirect(to="/listar")
+        else:
+            data["form"] = formulario   
+    return render(request, 'producto/agregar.html', data)
+
+
+def listarProductos(request):
+    busqueda = request.POST.get("buscador")
+    lista_productos = Producto.objects.order_by('nombre')
+    if busqueda:
+        lista_productos = Producto.objects.filter(
+            Q(nombre__icontains = busqueda) |
+            Q(descripcion__icontains = busqueda)
+        ).distinct()
+    try:
+        lista_productos = lista_productos
+    except:
+        raise Http404
+
+    data = {'entity': lista_productos,
+            'title': 'LISTADO DE PRODUCTOS',
+            }
+    return render(request, 'producto/listar.html', data)
+
