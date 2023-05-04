ac8e5f84e285260ec2efc2246271c8faede861a1
arizonv <a.vasgarridoe@gmail.com>
2023-05-04 02:40:12 -0400

    base - 2.2 carrito de compras test estilos

--- shopping/Carrito.py
+++ shopping/Carrito.py
@@ -10,20 +10,36 @@ class Carrito:
         else:
             self.carrito = carrito
             
-    def agregar(self, producto):
+    # def agregar(self, producto):
+    #     id = str(producto.id)
+    #     if id not in self.carrito.keys():
+    #         self.carrito[id]={
+    #             "producto_id": producto.id,
+    #             "nombre": producto.nombre,
+    #             "precio_uni":producto.precio,
+    #             "acumulado": producto.precio_descuento, # Utilizar el precio con descuento
+    #             "cantidad": 1,
+    #         }
+    #     else:
+    #         self.carrito[id]["cantidad"] += 1
+    #         self.carrito[id]["acumulado"] += producto.precio_descuento # Utilizar el precio con descuento
+    #     self.guardar_carrito()
+
+    def agregar(self, producto, cantidad=1):
         id = str(producto.id)
         if id not in self.carrito.keys():
             self.carrito[id]={
                 "producto_id": producto.id,
                 "nombre": producto.nombre,
                 "precio_uni":producto.precio,
-                "acumulado": producto.precio_descuento, # Utilizar el precio con descuento
-                "cantidad": 1,
+                "acumulado": producto.precio_descuento * cantidad, # Utilizar el precio con descuento
+                "cantidad": cantidad,
             }
         else:
-            self.carrito[id]["cantidad"] += 1
-            self.carrito[id]["acumulado"] += producto.precio_descuento # Utilizar el precio con descuento
+            self.carrito[id]["cantidad"] += cantidad
+            self.carrito[id]["acumulado"] += producto.precio_descuento * cantidad # Utilizar el precio con descuento
         self.guardar_carrito()
+
 
 
     def guardar_carrito(self):
